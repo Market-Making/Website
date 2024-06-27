1e2d42ba678b4ce17bcae3745e815643779929f8
Joylimjy <joylimjiayin@gmail.com>
2024-06-27 15:21:39 +0800

    add SNRN

--- src/pages/MM/index.tsx
+++ src/pages/MM/index.tsx
@@ -439,7 +439,7 @@ const MM = (props: any) => {
           })}
         </div>}
         {activeStrategy == 'MEXC' && <div style={{ float: 'left', display: 'flex', marginTop: 20 }}>
-          {['SEND', 'GRE'].map(coin => {
+          {['SEND', 'GRE', 'SNRN'].map(coin => {
             return <span
               style={{ cursor: 'pointer', fontFamily: 'unset', color: activeCoin == coin ? 'white' : '#ffffffb3', marginRight: 20 }}
               onClick={() => { setActiveCoin(coin) }}
